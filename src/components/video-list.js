1fa50db91425ea2f73d3c44be2bb78a1bc195b07
Brian Dudey <dudey.brian@gmail.com>
2018-03-12 19:22:22 -0500

    sixth commit

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -2,21 +2,32 @@ import React from 'react';
 import { fetchVideos } from '../actions/video';
 import { connect } from 'react-redux';
 import { Player } from 'video-react';
-
+import PDFReader from 'react-pdf-reader';
+import '../../node_modules/video-react/dist/video-react.css';
 export class VideoList extends React.Component {
 	componentDidMount() {
 		this.props.dispatch(fetchVideos());
 	}
 
 	render() {
-		console.log('string', this.props.videos);
+		console.log('string', this.props.videos[0]);
 		const videos =
 			this.props.videos &&
-			this.props.videos.map((video, index) => <div key={index}>{video}</div>);
+			this.props.videos.map((video, index) => (
+				<Player key={index} playsInline src={video} />
+			));
 
 		return (
 			<div>
-				<Player playsInline src="{videos}" />
+				{videos} {/* <ul>
+					<Player playsInline src="{videos}" />
+				</ul> */}
+				{/* <div>
+					<PDFReader
+						file="http://www.buddhanet.net/pdf_file/words_of_buddha.pdf"
+						renderType="canvas"
+					/>
+				</div> */}
 			</div>
 		);
 	}
